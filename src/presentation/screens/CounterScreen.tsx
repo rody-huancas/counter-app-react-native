ef028217b2ee6d606dce7a95b956c22d6832faf2
Rody Huancas <rodyhuancas.04@gmail.com>
2025-01-03 22:17:17 -0500

    Feat: button component

--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,5 +1,6 @@
 import {useState} from 'react';
-import {Platform, Pressable, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {PrimaryButton} from '../components';
 
 export const CounterScreen = () => {
   const [count, setCount] = useState(10);
@@ -8,17 +9,11 @@ export const CounterScreen = () => {
     <View style={styles.container}>
       <Text style={styles.title}>{count}</Text>
 
-      <Pressable
+      <PrimaryButton
+        label="Incrementar"
         onPress={() => setCount(count + 1)}
         onLongPress={() => setCount(0)}
-        style={({pressed}) => [styles.button, pressed && styles.buttonPressed]}>
-        <Text
-          style={{
-            color: Platform.OS === 'android' ? 'white' : '#4746AB',
-          }}>
-          Incrementar
-        </Text>
-      </Pressable>
+      />
     </View>
   );
 };
@@ -33,14 +28,5 @@ const styles = StyleSheet.create({
     fontSize: 80,
     color: 'black',
     fontWeight: '300',
-  },
-  button: {
-    backgroundColor: Platform.OS === 'android' ? '#5856D6' : 'white',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 10,
-  },
-  buttonPressed: {
-    backgroundColor: Platform.OS === 'android' ? '#4746AB' : 'white',
   },
 });
